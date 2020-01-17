d95c3edd431445bf3a8aeaee65d5234a0523c1ed
Antoine Bretaire <58423730+abretaire@users.noreply.github.com>
2020-01-17 13:44:35 +0100

    Add files via upload

---
+++ Article.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicePOO
+{
+    public class Article
+    {
+        private string reference;
+        public string Reference
+        {
+            get
+            {
+                return reference;
+            }
+            set
+            {
+                reference = value;
+            }
+        }
+
+
+        private string Designation { get; set; }
+
+        private double prixHT;
+        public double PrixHT { get => prixHT; set => prixHT = value; }
+        
+        
+        private double tauxTVA;
+        public double TauxTVA { get => tauxTVA; set => tauxTVA = value; }
+
+
+        public Article()
+        {
+
+        }
+        public Article(string reference, string designation, double prixHT, double tauxTVA)
+        {
+            this.reference = reference;
+            this.Designation = designation;
+            this.prixHT = prixHT;
+            this.tauxTVA = tauxTVA;
+        }
+
+        public double CalculerPrixTTC()
+        {
+            double prixTTC =  0;
+
+            prixTTC = prixHT + (prixHT * tauxTVA / 100);
+
+            return prixTTC;
+        }
+
+        public void Afficher()
+        {
+
+            Console.WriteLine(string.Format("Reference: {0}", this.reference));
+            Console.WriteLine(string.Format("Designation: {0}", this.Designation));
+            Console.WriteLine(string.Format("PrixHT: {0}", this.prixHT));
+            Console.WriteLine(string.Format("tauxTVA: {0}", this.tauxTVA));
+            Console.WriteLine(string.Format("PrixTTC: {0}", CalculerPrixTTC()));
+        }
+
+        
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicePOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Article article1 = new Article();
+            article1.Afficher();
+
+            Article article2 = new Article("Article 1", "Désignation de l'article 1", 100, 20);
+            article2.Afficher();
+
+            Console.ReadLine();
+        }
+    }
+}
